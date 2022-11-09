9d98967c1fae8d1287055d3952e05c72c8e7dfde
Azimjon <95956084+Azimjon-exe@users.noreply.github.com>
2022-11-09 21:56:46 +0500

    a

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,86 +1,77 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Search extends Component {
-  state = {
-    search: "panda",
-    type: "all",
-  };
-  handelKey = (e) => {
+export default function Search(props) {
+  const { searchMovie } = props;
+
+  const [search, setSearch] = useState("panda");
+  const [type, setType] = useState("all");
+
+  const handelKey = (e) => {
     if (e.key === "Enter") {
-      this.props.searchMovie(this.state.search, this.state.type);
+      searchMovie(search, type);
     }
   };
 
-  hendlefilter = (e) => {
-    this.setState(
-      () => ({ type: e.target.dataset.type }),
-      () => {
-        this.props.searchMovie(this.state.search, this.state.type);
-      }
-    );
+  const hendlefilter = (e) => {
+    setType(e.target.dataset.type);
+    searchMovie(search, e.target.dataset.type);
   };
 
-  render() {
-    return (
-      <div>
-        <div className="col s12">
-          <div className="input-field ">
-            <input
-              placeholder="Search"
-              type="search"
-              className="validate"
-              value={this.state.search}
-              onChange={(e) => this.setState({ search: e.target.value })}
-              onKeyDown={this.handelKey}
-            />
-            <button
-              className="btn search-btn"
-              onClick={() =>
-                this.props.searchMovie(this.state.search, this.state.type)
-              }
-            >
-              Search Movies
-            </button>
-          </div>
-        </div>
-        <div>
-          <label>
-            <input
-              class="with-gap"
-              name="group3"
-              type="radio"
-              data-type="all"
-              onChange={this.hendlefilter}
-              checked={this.state.type === "all"}
-            />
-            <span>All</span>
-          </label>
-          <label>
-            <input
-              class="with-gap"
-              name="group3"
-              type="radio"
-              data-type="movie"
-              onChange={this.hendlefilter}
-              checked={this.state.type === "movie"}
-            />
-            <span>Movies only</span>
-          </label>
-          <label>
-            <input
-              class="with-gap"
-              name="group3"
-              type="radio"
-              data-type="series"
-              onChange={this.hendlefilter}
-              checked={this.state.type === "series"}
-            />
-            <span>Series only</span>
-          </label>
+  return (
+    <div>
+      <div className="col s12">
+        <div className="input-field ">
+          <input
+            placeholder="Search"
+            type="search"
+            className="validate"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handelKey}
+          />
+          <button
+            className="btn search-btn"
+            onClick={() => searchMovie(search, type)}
+          >
+            Search Movies
+          </button>
         </div>
       </div>
-    );
-  }
+      <div>
+        <label>
+          <input
+            class="with-gap"
+            name="group3"
+            type="radio"
+            data-type="all"
+            onChange={hendlefilter}
+            checked={type === "all"}
+          />
+          <span>All</span>
+        </label>
+        <label>
+          <input
+            class="with-gap"
+            name="group3"
+            type="radio"
+            data-type="movie"
+            onChange={hendlefilter}
+            checked={type === "movie"}
+          />
+          <span>Movies only</span>
+        </label>
+        <label>
+          <input
+            class="with-gap"
+            name="group3"
+            type="radio"
+            data-type="series"
+            onChange={hendlefilter}
+            checked={type === "series"}
+          />
+          <span>Series only</span>
+        </label>
+      </div>
+    </div>
+  );
 }
-
-export default Search;
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,42 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loader from "../components/Loader";
 import Movies from "../components/Movies";
 import Search from "../components/Search";
 
-class Main extends React.Component {
-  state = {
-    movies: [],
-    loading: true,
-  };
+function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    fetch("http://www.omdbapi.com/?apikey=8b1d8865&s=panda")
-      .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
-  }
-
-  searchMovies = (str, type = "all") => {
-    this.setState({ loading: true });
+  const searchMovies = (str, type = "all") => {
+    setLoading(true);
     fetch(
       `http://www.omdbapi.com/?apikey=8b1d8865&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => {
+        setLoading(false);
+        setMovies(data.Search);
+      });
   };
-  render() {
-    return (
-      <div className="container content">
-        <Search searchMovie={this.searchMovies} />
-        {this.state.loading ? (
-          <Loader />
-        ) : (
-          <Movies movies={this.state.movies} />
-        )}
-      </div>
-    );
-  }
+
+  useEffect(() => {
+    fetch("http://www.omdbapi.com/?apikey=8b1d8865&s=panda")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container content">
+      <Search searchMovie={searchMovies} />
+      {loading ? <Loader /> : <Movies movies={movies} />}
+    </div>
+  );
 }
 
 export default Main;
