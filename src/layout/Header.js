e712c253e6dff61a18878991009a2141c29c3a03
Azimjon <95956084+Azimjon-exe@users.noreply.github.com>
2023-01-03 11:44:46 +0500

    Alll

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,19 +6,8 @@ const Header = () => {
       <nav>
         <div className="nav-wrapper container">
           <a href="dvd.html" className="brand-logo">
-            React
+            Movies
           </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Sass</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li>
-          </ul>
         </div>
       </nav>
     </div>
